b7822bdd9c1c1350241164e34700ad99f1328a6e
Jonathan Christie <jchristie203@gmail.com>
2021-05-07 08:43:49 -0400

    route grades conversions finished

--- app/src/main/java/com/example/ClimbingGradeConverter/MainActivity.java
+++ app/src/main/java/com/example/ClimbingGradeConverter/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     int gradeIndex;
     String[] hueco = new String[] {"VB","V0", "V1", "V2", "V3" , "V4", "V5", "V6", "V7", "V8","V9","V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17" };
     String[] font = new String[] { "3", "4",  "5",   "5+","6A/6A+","6B/6B+","6C/6C+","7A","7A+","7B/7B+","7B+/7C","7C+","8A","8A+","8B","8B+","8C","8C+","9A"};
-
+    String[] YDS = new String [] {"5","5.1/5.2","5.3/5.4","5.5","5.6","5.7","5.8","5.9","5.10a","5.10b","5.10c","5.10d","5.11a","5.11b","5.11c","5.11d","5.12a","5.12b","5.12c","5.12d","5.13a","5.13b","5.13c","5.13d","5.14a","5.14b","5.14c","5.14d","5.15a","5.15b","5.15c","5.15d"};
+    String[] french = new String[] {"1","2","3","4a","4b","4c","5a","5b","5c","6a","6a+","6b","6b+","6c","6c+","7a","7a+","7b","7b+","7c","7c+","8a","8a+","8b","8b+","8c","8c+","9a","9a+","9b","9b+","9c"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,6 +130,10 @@ public class MainActivity extends AppCompatActivity {
                     result.setText(conversionChoice + " " + font[gradeIndex]);
                 } else if (conversionChoice.equals("Fontainebleau to Hueco")) {
                     result.setText(conversionChoice + " " + hueco[gradeIndex]);
+                } else if (conversionChoice.equals("YDS to French")){
+                    result.setText(conversionChoice + " " + french[gradeIndex]);
+                } else if (conversionChoice.equals("French to YDS")){
+                    result.setText(conversionChoice + " " + YDS[gradeIndex]);
                 }
             }
         });
